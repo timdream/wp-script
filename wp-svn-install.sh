8a11f7d0596ba5360de72577d9e93f3f00282360
Tim Chien [:timdream] <timdream@gmail.com>
2010-02-01 01:55:06 +0800

    Correct locale file path

--- wp-svn-install.sh
+++ wp-svn-install.sh
@@ -1,5 +1,5 @@
 svn co http://core.svn.wordpress.org/tags/$2 $1
-eval "svn propset svn:externals 'languages http://svn.automattic.com/wordpress-i18n/zh_TW/branches/"$3"/messages' "$1"/wp-content"
+eval "svn propset svn:externals 'languages http://svn.automattic.com/wordpress-i18n/zh_TW/tags/"$3"/messages' "$1"/wp-content"
 svn update $1/wp-content
 echo "
 \$wp_local_package = 'zh_TW';" >> $1/wp-includes/version.php
--- wp-svn-update.sh
+++ wp-svn-update.sh
@@ -1,3 +1,3 @@
 svn switch http://core.svn.wordpress.org/tags/$2 $1
-eval "svn propset svn:externals 'languages http://svn.automattic.com/wordpress-i18n/zh_TW/branches/"$3"/messages' "$1"/wp-content"
+eval "svn propset svn:externals 'languages http://svn.automattic.com/wordpress-i18n/zh_TW/tags/"$3"/messages' "$1"/wp-content"
 svn update $1/wp-content
