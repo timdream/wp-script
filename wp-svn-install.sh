4d9b79230a735b3936fac579ec78287c69e01d8e
Timothy Guan-tin Chien <timdream@gmail.com>
2011-01-10 11:44:43 +0800

    Rewrite scripts with version detection from API.

--- wp-svn-install.sh
+++ wp-svn-install.sh
@@ -1,5 +1,40 @@
-svn co http://core.svn.wordpress.org/tags/$2 $1
-eval "svn propset svn:externals 'languages http://svn.automattic.com/wordpress-i18n/zh_TW/tags/"$3"/messages' "$1"/wp-content"
+if [ -z $1 ]; then
+	echo 'Usage: '$0' [WordPress root dir]'
+	exit 1
+fi
+
+mkdir -p $1 > /dev/null
+
+VER_CHECK='http://api.wordpress.org/core/version-check/1.4/'
+LANG='zh_TW'
+
+echo
+echo '* Checking latest WordPress version...'
+WP_VER=`wget -q $VER_CHECK -O - | head -n 4 | tail -1`
+echo 'Latest WordPress version: '$WP_VER
+L10N_VER=`wget -q $VER_CHECK?locale=$LANG -O - | head -n 4 | tail -1`
+echo 'Latest '$LANG' version: '$L10N_VER
+echo
+
+echo '* Installing WordPress files through subversion...'
+svn co http://core.svn.wordpress.org/tags/$WP_VER $1
+echo
+
+echo '* Verify the existence of l10n repo with tagged version...'
+L10N_REPO=http://svn.automattic.com/wordpress-i18n/$LANG/tags/$L10N_VER/messages
+wget $L10N_REPO -q -O - > /dev/null
+if [ $? -ne 0 ]; then
+	echo 'Warning: l10n repo with '$L10N_VER' tag does not exist, using files in trunk instead.'
+	L10N_REPO=http://svn.automattic.com/wordpress-i18n/$LANG/trunk/messages
+fi
+echo
+
+echo '* Installing l10n files through subversion...'
+eval "svn propset svn:externals 'languages $L10N_REPO' "$1"/wp-content"
 svn update $1/wp-content
+echo
+
 echo "
-\$wp_local_package = 'zh_TW';" >> $1/wp-includes/version.php
+\$wp_local_package = '$LANG';" >> $1/wp-includes/version.php
+
+echo 'Done.'
--- wp-svn-update.sh
+++ wp-svn-update.sh
@@ -1,3 +1,42 @@
-svn switch http://core.svn.wordpress.org/tags/$2 $1
-eval "svn propset svn:externals 'languages http://svn.automattic.com/wordpress-i18n/zh_TW/tags/"$3"/messages' "$1"/wp-content"
+if [ -z $1 ]; then
+	echo 'Usage: '$0' [WordPress root dir]'
+	exit 1
+fi
+
+if [ ! -d $1/.svn ]; then
+	echo 'Error: '$1' is not a subversion-managed WordPress.'
+	echo 'Please install WordPress with wp-svn-install at first place.'
+	echo 'You could convert your tarball WordPress into svn WordPress by copying config and uploaded/changed files into it.'
+	exit 1
+fi
+
+VER_CHECK='http://api.wordpress.org/core/version-check/1.4/'
+LANG='zh_TW'
+
+echo '* Checking latest WordPress version...'
+WP_VER=`wget -q $VER_CHECK -O - | head -n 4 | tail -1`
+L10N_VER=`wget -q $VER_CHECK?locale=$LANG -O - | head -n 4 | tail -1`
+
+echo 'Latest WordPress version: '$WP_VER
+echo 'Latest '$LANG' version: '$L10N_VER
+echo
+
+echo '* Upgrading WordPress files through subversion...'
+svn switch http://core.svn.wordpress.org/tags/$WP_VER $1
+echo
+
+echo '* Verify the existence of l10n repo with tagged version...'
+L10N_REPO=http://svn.automattic.com/wordpress-i18n/$LANG/tags/$L10N_VER/messages
+wget $L10N_REPO -q -O - > /dev/null
+if [ $? -ne 0 ]; then
+	echo 'Warning: l10n repo with '$L10N_VER' tag does not exist, using files in trunk instead.'
+	L10N_REPO=http://svn.automattic.com/wordpress-i18n/$LANG/trunk/messages
+fi
+echo
+
+echo '* Upgrading l10n files through subversion...'
+eval "svn propset svn:externals 'languages $L10N_REPO' "$1"/wp-content"
 svn update $1/wp-content
+echo
+
+echo 'Done.'
