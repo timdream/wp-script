3444de51efc4ce4a02e0ad2b27fb83c00a39e98b
Timothy Guan-tin Chien <timdream@gmail.com>
2013-02-03 20:15:41 +0800

    Make WP_LANG optional and not hardcoded

--- wp-svn-install.sh
+++ wp-svn-install.sh
@@ -12,35 +12,41 @@ if [ -z $PROTOCOL ]; then
 fi
 
 VER_CHECK=${PROTOCOL}'://api.wordpress.org/core/version-check/1.4/'
-WP_LANG='zh_TW'
 
 echo
 echo '* Checking latest WordPress version...'
 WP_VER=`wget -q $VER_CHECK -O - | head -n 4 | tail -1`
 echo 'Latest WordPress version: '$WP_VER
-L10N_VER=`wget -q $VER_CHECK?locale=$WP_LANG -O - | head -n 4 | tail -1`
-echo 'Latest '$WP_LANG' version: '$L10N_VER
+
+if [ ! -z $WP_LANG ]; then
+  L10N_VER=`wget -q $VER_CHECK?locale=$WP_LANG -O - | head -n 4 | tail -1`
+  echo 'Latest '$WP_LANG' version: '$L10N_VER
+fi
+
 echo
 
 echo '* Installing WordPress files through subversion...'
 svn co ${PROTOCOL}://core.svn.wordpress.org/tags/$WP_VER $1
 echo
 
-echo '* Verify the existence of l10n repo with tagged version...'
-L10N_REPO=${PROTOCOL}://svn.automattic.com/wordpress-i18n/$WP_LANG/tags/$L10N_VER/messages
-wget $L10N_REPO -q -O - > /dev/null
-if [ $? -ne 0 ]; then
-	echo 'Warning: l10n repo with '$L10N_VER' tag does not exist, using files in trunk instead.'
-	L10N_REPO=${PROTOCOL}://svn.automattic.com/wordpress-i18n/$WP_LANG/trunk/messages
+if [ ! -z $WP_LANG ]; then
+  echo '* Verify the existence of l10n repo with tagged version...'
+  L10N_REPO=${PROTOCOL}://svn.automattic.com/wordpress-i18n/$WP_LANG/tags/$L10N_VER/messages
+  wget $L10N_REPO -q -O - > /dev/null
+  if [ $? -ne 0 ]; then
+    echo 'Warning: l10n repo with '$L10N_VER' tag does not exist, using files in trunk instead.'
+    L10N_REPO=${PROTOCOL}://svn.automattic.com/wordpress-i18n/$WP_LANG/trunk/messages
+  fi
+  echo
+
+  echo '* Installing l10n files through subversion...'
+  eval "svn propset svn:externals 'languages $L10N_REPO' "$1"/wp-content"
+  svn update $1/wp-content
+
+  echo '* Patch version.php to register the locale installed.'
+  echo "
+  \$wp_local_package = '$WP_LANG';" >> $1/wp-includes/version.php
+  echo
 fi
-echo
-
-echo '* Installing l10n files through subversion...'
-eval "svn propset svn:externals 'languages $L10N_REPO' "$1"/wp-content"
-svn update $1/wp-content
-echo
-
-echo "
-\$wp_local_package = '$WP_LANG';" >> $1/wp-includes/version.php
 
 echo 'Done.'
--- wp-svn-upgrade.sh
+++ wp-svn-upgrade.sh
@@ -17,32 +17,39 @@ if [ -z $PROTOCOL ]; then
 fi
 
 VER_CHECK=${PROTOCOL}'://api.wordpress.org/core/version-check/1.4/'
-WP_LANG='zh_TW'
+if [ ! -z $WP_LANG ]; then
+  WP_LANG=`echo 'echo $wp_local_package;' | cat $1/wp-includes/version.php - | php 2>/dev/null`
+fi
 
 echo '* Checking latest WordPress version...'
 WP_VER=`wget -q $VER_CHECK -O - | head -n 4 | tail -1`
-L10N_VER=`wget -q $VER_CHECK?locale=$WP_LANG -O - | head -n 4 | tail -1`
-
 echo 'Latest WordPress version: '$WP_VER
-echo 'Latest '$WP_LANG' version: '$L10N_VER
+
+if [ ! -z $WP_LANG ]; then
+  L10N_VER=`wget -q $VER_CHECK?locale=$WP_LANG -O - | head -n 4 | tail -1`
+  echo 'Latest '$WP_LANG' version: '$L10N_VER
+fi
+
 echo
 
 echo '* Upgrading WordPress files through subversion...'
 svn switch ${PROTOCOL}://core.svn.wordpress.org/tags/$WP_VER $1
 echo
 
-echo '* Verify the existence of l10n repo with tagged version...'
-L10N_REPO=${PROTOCOL}://svn.automattic.com/wordpress-i18n/$WP_LANG/tags/$L10N_VER/messages
-wget $L10N_REPO -q -O - > /dev/null
-if [ $? -ne 0 ]; then
-	echo 'Warning: l10n repo with '$L10N_VER' tag does not exist, using files in trunk instead.'
-	L10N_REPO=${PROTOCOL}://svn.automattic.com/wordpress-i18n/$WP_LANG/trunk/messages
-fi
-echo
+if [ ! -z $WP_LANG ]; then
+  echo '* Verify the existence of l10n repo with tagged version...'
+  L10N_REPO=${PROTOCOL}://svn.automattic.com/wordpress-i18n/$WP_LANG/tags/$L10N_VER/messages
+  wget $L10N_REPO -q -O - > /dev/null
+  if [ $? -ne 0 ]; then
+    echo 'Warning: l10n repo with '$L10N_VER' tag does not exist, using files in trunk instead.'
+    L10N_REPO=${PROTOCOL}://svn.automattic.com/wordpress-i18n/$WP_LANG/trunk/messages
+  fi
+  echo
 
-echo '* Upgrading l10n files through subversion...'
-eval "svn propset svn:externals 'languages $L10N_REPO' "$1"/wp-content"
-svn update $1/wp-content
-echo
+  echo '* Upgrading l10n files through subversion...'
+  eval "svn propset svn:externals 'languages $L10N_REPO' "$1"/wp-content"
+  svn update $1/wp-content
+  echo
+fi
 
 echo 'Done.'
