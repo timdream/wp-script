eed4a5075f12d802f27a2f8137de466c62e4ed52
Timothy Guan-tin Chien <timdream@gmail.com>
2014-09-26 10:56:23 +0800

    set plugins/themes with svn:externals

---
+++ wp-svn-set-themes.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+if [ -z $1 ]; then
+  echo 'Usage: '$0' [WordPress root dir]'
+  exit 1
+fi
+
+WP_DIR=$1
+
+if [ -z $EDITOR ]; then
+  EDITOR=vim
+fi
+
+if [ -z $TMPDIR ]; then
+  TMPDIR=/tmp/
+fi
+
+svn propget svn:externals ${WP_DIR}/wp-content/themes > ${TMPDIR}externals.list
+
+$EDITOR ${TMPDIR}externals.list
+
+eval "svn propset svn:externals '`cat ${TMPDIR}externals.list`' '${WP_DIR}'/wp-content/themes"
+svn update ${WP_DIR}/wp-content/themes
